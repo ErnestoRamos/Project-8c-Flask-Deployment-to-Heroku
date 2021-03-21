23c1bde6d689f8f338d7e9d10282711000daf94d
ErnestoRamos <56518821+ErnestoRamos@users.noreply.github.com>
2021-03-21 15:10:59 -0400

    Add files via upload

---
+++ app.py
@@ -0,0 +1,31 @@
+from flask import Flask
+from flask import request, jsonify, render_template
+import pickle
+
+
+app = Flask(__name__)
+model = pickle.load(open('model.pkl', 'rb'))
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+
+@app.route('/predict', methods=['POST'])
+def predict():
+
+    '''
+    For rendering results on HTML GUI
+    '''
+    int_features = [int(x) for x in request.form.values()]
+    final_features = [np.array(int_features)]
+    prediction = model.predict(final_features)
+
+    output = round(prediction[0], 2)
+
+    return render_template('index.html', prediction_text='Employee Salary should be $ {}'.format(output))
+
+    # return 'Flask Dockerized'
+
+if __name__ == "__main__":
+    app.run(debug=True, host='0.0.0.0')
